c5c998064cfcdb16f390302b15855b586143d7ce
npross <npross@lbl.gov>
2017-04-17 16:16:18 +0100

    wealth_and_population/ directory added

---
+++ wealth_and_population/population_barchart.py
@@ -0,0 +1,12 @@
+'''
+https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)
+'''
+
+import pandas as pd
+import matplotlib as plt
+import seaborn
+
+
+pops = pd.read_html('https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)')
+
+
