f52afa3f2263213c9a7a266939450f97008365ec
npross <npross@lbl.gov>
2017-05-09 18:34:18 +0100

    %timeit added

--- fits_tests/FITS_fileIO_speedtest.py
+++ fits_tests/FITS_fileIO_speedtest.py
@@ -1,7 +1,11 @@
 from astropy.table import Table
 import fitsio
 import time
+import timeit
 import pyfits
+
+## Could also use
+https://docs.python.org/2/library/timeit.html
 
 start_time = time.time()
 
@@ -10,9 +14,9 @@ fname_decals = '/cos_pc19a_npr/data/DECaLS/legacysurvey/dr3/external/survey-dr3-
 
 
 columns = ['OBJID']
-#str = fitsio.read(fname_decals, columns=columns)
-#str = Table.read('fname_decals', columns=columns)
-fx = pyfits.open(fname_decals, memmap=True)
+timeit.timeit('str = fitsio.read(fname_decals, columns=columns), number=10)
+timeit.timeit('str = Table.read('fname_decals', columns=columns), number=10)
+timeit.timeit('fx = pyfits.open(fname_decals, memmap=True)', number=10
 
 
 print("--- %s seconds ---" % (time.time() - start_time))
