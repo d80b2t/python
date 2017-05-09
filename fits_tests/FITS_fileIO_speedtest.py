549fe472a794efe22b40ef4ed85b4b181cda6410
npross <npross@lbl.gov>
2017-05-09 20:09:32 +0100

    FITS_fileIO_speedtest.py

--- fits_tests/FITS_fileIO_speedtest.py
+++ fits_tests/FITS_fileIO_speedtest.py
@@ -4,20 +4,22 @@ import time
 import timeit
 import pyfits
 
-## Could also use
-https://docs.python.org/2/library/timeit.html
+## Can also use::
+## https://docs.python.org/2/library/timeit.html
 
 start_time = time.time()
 
 ##  5.4G  survey-dr3-specObj-dr13.fits
 fname_decals = '/cos_pc19a_npr/data/DECaLS/legacysurvey/dr3/external/survey-dr3-specObj-dr13.fits'
 
-
 columns = ['OBJID']
-timeit.timeit('str = fitsio.read(fname_decals, columns=columns), number=10)
-timeit.timeit('str = Table.read('fname_decals', columns=columns), number=10)
-timeit.timeit('fx = pyfits.open(fname_decals, memmap=True)', number=10
+#timeit.timeit('str = fitsio.read(fname_decals, columns=columns)', number=10)
+#timeit.timeit('str = Table.read(fname_decals, columns=columns)', number=10)
+#timeit.timeit('fx = pyfits.open(fname_decals, memmap=True)', number=10)
 
+#str = fitsio.read(fname_decals, columns=columns)
+#str = Table.read(fname_decals, columns=columns)
+str = pyfits.open(fname_decals, memmap=True)
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
